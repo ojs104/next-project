8f8acec9962b3a3116d431cd2184a379b6a3e77a
ojs104 <kmdojs@gmail.com>
2023-11-13 10:22:04 +0900

    11-13 first

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,28 @@
+import React from 'react'
+import Contact from '@/components/Contact'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import Intro from '@/components/Intro'
+import Port from '@/components/Port'
+import Site from '@/components/Site'
+import Skill from '@/components/Skill'
+import Skip from '@/components/Skip'
+
 export default function Home() {
   return (
-    <div>next.js</div>
+    <>
+      <Skip />
+      <Header />
+      <main id='main' role='main'>
+        <Intro />
+        <Skill />
+        <Site />
+        <Port />
+        <Contact />
+      </main>
+      <Footer />
+
+    </>
+
   )
 }
